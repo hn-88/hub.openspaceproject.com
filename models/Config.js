592070726829b6eb7a6eff3533cede1111f0a9ec
hn-88 <hn-88@users.noreply.github.com>
2025-08-25 13:42:17 +0530

    Update Config.js with 0.21.2 to 0.21.0

--- models/Config.js
+++ models/Config.js
@@ -15,6 +15,9 @@ const configSchema = new mongoose.Schema({
     type: Array,
     required: true,
     default: [
+      '0.21.2',
+      '0.21.1',
+      '0.21.0',
       '0.20.1',
       '0.20.0',
       '0.19.2',
